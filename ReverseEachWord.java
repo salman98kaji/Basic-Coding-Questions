696ba1594b641f947a12e699f19a51bd75af4461
salman98kaji <salman2017kaji@gmail.com>
2024-12-18 18:18:20 +0530

    de

--- ReverseEachWord.java
+++ ReverseEachWord.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ReverseEachWord {
+public class ReverseEachWord {
+    public static void main(String[] args){
+        String str = "Hello world salman";
+        String[] words = str.split(" ");
+        StringBuilder reversed = new StringBuilder();
+        for(String word: words){
+            StringBuilder sb = new StringBuilder(word);
+            reversed.append(sb.reverse().toString()).append(" ");
+        }
+        System.out.println("Reversed Each Word: " + reversed.toString().trim());
+    }
+
 }
