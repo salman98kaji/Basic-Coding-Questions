24d341c4a94f6b304dfcc8c36b7ab56fd3ff2066
salman98kaji <salman2017kaji@gmail.com>
2024-12-18 17:36:58 +0530

    3

---
+++ ReverseWords.java
@@ -0,0 +1,14 @@
+public class ReverseWords {
+    public static void main(String[] args){
+        String str = "Hello world how are you";
+        String[] words = str.split(" ");
+        StringBuilder reversed = new StringBuilder();
+
+        for(int i=words.length-1; i>0; i--){
+            reversed.append(words[i]).append(" ");
+        }
+
+        System.out.println("reversed words :"+reversed.toString().trim());
+    }
+
+}
