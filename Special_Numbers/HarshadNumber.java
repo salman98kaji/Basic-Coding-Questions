a4e3afaa3764f5d4cd221ebf2d05e1c0a9c48bf6
salman98kaji <salman2017kaji@gmail.com>
2024-12-19 16:20:48 +0530

    strong number

---
+++ Special_Numbers/HarshadNumber.java
@@ -0,0 +1,31 @@
+package Special_Numbers;
+
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isHarshadNumber(number)) {
+            System.out.println(number + " is a Harshad number.");
+        } else {
+            System.out.println(number + " is not a Harshad number.");
+        }
+    }
+
+    public static boolean isHarshadNumber(int num){
+        int originalNum = num;
+        int sumOfDigits = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sumOfDigits += digit;
+            num /= 10;
+        }
+        return originalNum % sumOfDigits == 0;
+    }
+}
+
+//Harshad number is a number or an integer in base 10 which is completely divisible by sum of  its digits.
